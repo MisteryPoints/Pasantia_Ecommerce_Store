edc3944900738c9d22a97684669456c406aca0ca
MisteryPoints <misterypoint2@gmail.com>
2023-10-22 16:08:14 -0400

    fixing things

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -16,12 +16,12 @@ const Summary = () => {
 
   useEffect(() => {
     if (searchParams.get('success')) {
-      toast.success('Payment completed.');
+      toast.success('Pago completado.');
       removeAll();
     }
 
     if (searchParams.get('canceled')) {
-      toast.error('Something went wrong.');
+      toast.error('Algo malo ha sucedido.');
     }
   }, [searchParams, removeAll]);
 
@@ -42,16 +42,16 @@ const Summary = () => {
       className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
       <h2 className="text-lg font-medium text-gray-900">
-        Order summary
+        Detalle de la Orden
       </h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order total</div>
+          <div className="text-base font-medium text-gray-900">Total de la Orden</div>
          <Currency value={totalPrice} />
         </div>
       </div>
       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
-        Checkout
+        Establecer Compra
       </Button>
     </div>
   );
